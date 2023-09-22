4ba95f4a24409e2d7475e411e92efaffe7f52615
Alberto clase <DAM2A-03@JOVELLANOS.EDU>
2023-09-22 14:05:23 +0200

    falta menu

--- ProyectosDatos/AlbertoColmenarOperacionesConNio/src/albertocolmenaroperacionesconnio/AlbertoColmenarOperacionesConNio.java
+++ ProyectosDatos/AlbertoColmenarOperacionesConNio/src/albertocolmenaroperacionesconnio/AlbertoColmenarOperacionesConNio.java
@@ -1,9 +1,10 @@
 package albertocolmenaroperacionesconnio;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 /**
  *
@@ -15,11 +16,15 @@ public class AlbertoColmenarOperacionesConNio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        File ruta1 = new File("../prueba.txt");
-        File ruta2 = new File("../pruebaACopiar.txt");
-        copiarFichero(ruta1.toPath(), ruta2.toPath());
+        Path ruta1 = Paths.get("C:/Users/DAM2A-03/Documents/Datos/ProyectosDatos/prueba.txt");
+        Path ruta2 = Paths.get("C:/Users/DAM2A-03/Documents/Datos/ProyectosDatos/qwe/prueba2.txt");
+        //copiarFichero(ruta1, ruta2);
+        //moverFichero(ruta1, ruta2);
+        //mostrarFichero(ruta1);
+        mostrarInformacionFichero(ruta1);
         
     }
+    
     public static void copiarFichero(Path ficheroACopiar, Path ficheroCopia) {
         try {
             if (Files.exists(ficheroACopiar)) {
@@ -29,6 +34,54 @@ public class AlbertoColmenarOperacionesConNio {
             }
         } catch(IOException ex) {
             System.out.println("Error al copiar los ficheros" + ex);
+        }
+    }
+    
+    public static void moverFichero(Path fichero, Path destino) {
+        try {
+            if (Files.exists(fichero)) {
+                Files.move(fichero, destino);
+            } else {
+                System.out.format("El fichero %s no existe", fichero.getFileName());
+            }
+        } catch(IOException ex) {
+            System.out.println("Error al mover el fichero" + ex);
+        }
+    }
+    
+    public static void mostrarFichero(Path fichero) {
+        try {
+            if (Files.exists(fichero)) {
+                List<String> lineas = Files.readAllLines(fichero);
+                for(String linea : lineas) {
+                    System.out.println(linea);
+                }
+            } else {
+                System.out.format("El fichero %s no existe", fichero.getFileName());
+            }
+        }
+        catch(IOException ex) {
+            System.out.println("Error en el fichero" + ex);
+        }
+    }
+        
+    public static void mostrarInformacionFichero(Path fichero) {
+        try {
+            if (Files.exists(fichero)) {
+                String propietario = Files.getOwner(fichero).toString();
+                boolean permisoLectura = Files.isReadable(fichero);
+                boolean esEjecutable = Files.isExecutable(fichero);
+                boolean esOculto = Files.isHidden(fichero);
+                System.out.format("El propietario es %s%n"
+                        + "¿Tiene permiso de lectura? %b%n"
+                        + "¿Es ejecutable? %b%n"
+                        + "¿Es oculto? %b%n", propietario, permisoLectura, esEjecutable, esOculto);
+            } else {
+                System.out.format("El fichero %s no existe", fichero.getFileName());
+            }
+        }
+        catch(IOException ex) {
+            System.out.println("Error en el fichero" + ex);
         }
     }
 }
