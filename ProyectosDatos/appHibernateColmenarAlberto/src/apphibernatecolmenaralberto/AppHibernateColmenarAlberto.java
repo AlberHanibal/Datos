8665fafde89daf55a4eb0c10c49486ac2726dba6
Alber clase <alber@jovellanos>
2023-12-21 10:53:40 +0100

    fin clase

--- ProyectosDatos/appHibernateColmenarAlberto/src/apphibernatecolmenaralberto/AppHibernateColmenarAlberto.java
+++ ProyectosDatos/appHibernateColmenarAlberto/src/apphibernatecolmenaralberto/AppHibernateColmenarAlberto.java
@@ -1,9 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package apphibernatecolmenaralberto;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 /**
  *
@@ -11,11 +13,62 @@ package apphibernatecolmenaralberto;
  */
 public class AppHibernateColmenarAlberto {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private SessionFactory sesion;
+    
+    public AppHibernateColmenarAlberto() {
+        sesion = SessionFactoryUtil.getSessionFactory();
     }
     
+    public void insertarVuelo(String codVuelo, String horaSalida, String destino, 
+            String procedencia, Integer plazasFumador, Integer plazasNoFumador, 
+            Integer plazasTurista, Integer plazasPrimera) {
+        
+        Session session = sesion.openSession();
+        Transaction transaction = session.beginTransaction();
+        Vuelos vuelo = new Vuelos(codVuelo, horaSalida, destino, procedencia, plazasFumador, 
+                plazasNoFumador, plazasTurista, plazasPrimera);
+        
+        session.save(vuelo);
+        transaction.commit();
+        session.close();
+    }
+    
+    public List obtenerListaVuelos() {
+        Session session = sesion.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query q = session.createQuery("from Vuelos");
+        List <Vuelos> listaVuelo = q.list();
+        session.close();
+        return listaVuelo;
+    }
+    
+    public Vuelos obtenerVuelo(String codVuelo) {
+        Session session = sesion.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query q = session.createQuery("from Vuelos where codVuelo = '" + codVuelo + "'");
+        Vuelos vuelo = (Vuelos) q.uniqueResult();
+        return vuelo;
+    }
+    
+    public void modificarVuelo(String codVuelo, String horaSalida, String destino, 
+            String procedencia, Integer plazasFumador, Integer plazasNoFumador, 
+            Integer plazasTurista, Integer plazasPrimera) {
+        
+        Session session = sesion.openSession();
+        Transaction transaction = session.beginTransaction();
+        
+        Vuelos vuelo = (Vuelos) session.load(Vuelos.class, codVuelo);
+        vuelo.setDestino(destino);
+        vuelo.setHoraSalida(horaSalida);
+        vuelo.setProcedencia(procedencia);
+        vuelo.setPlazasFumador(plazasFumador);
+        vuelo.setPlazasNoFumador(plazasNoFumador);
+        vuelo.setPlazasTurista(plazasTurista);
+        vuelo.setPlazasPrimera(plazasPrimera);
+        
+        session.update(vuelo);       
+        session.save(vuelo);
+        transaction.commit();
+        session.close();
+    }
 }
