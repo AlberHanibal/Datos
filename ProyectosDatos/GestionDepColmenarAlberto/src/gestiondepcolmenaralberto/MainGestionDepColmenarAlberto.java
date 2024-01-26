5d1b7d14343f96a7991d165682891cd1c5643992
Alber clase <alber@jovellanos>
2024-01-26 14:37:47 +0100

    fin

---
+++ ProyectosDatos/GestionDepColmenarAlberto/src/gestiondepcolmenaralberto/MainGestionDepColmenarAlberto.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestiondepcolmenaralberto;
+
+/**
+ *
+ * @author DAM2A-03
+ */
+public class MainGestionDepColmenarAlberto {
+    
+    public static void main(String[] args) {
+        
+        GestionDepColmenarAlberto GestionDep = new GestionDepColmenarAlberto();
+        // GestionDep.insertar("SILLAS", "FUENLABRADA");
+        GestionDep.visualizarPorNombre("SALES");
+    }
+}
